4872258f11522d51260d7723f4b935fe3f707947
Justin Li <justinnhli@gmail.com>
2017-02-28 16:05:32 -0800

    add incorrect binding interpreter

--- pylite.py
+++ pylite.py
@@ -80,10 +80,54 @@ class StochasticPyliteInterpreter(PyliteInterpreter):
     def run(code):
         StochasticPyliteInterpreter().visit(parse(code))
 
+class BindingInterpreter(PyliteInterpreter):
+    def __init__(self):
+        super().__init__()
+    def visit_BinOp(self, node):
+        # check that this is an Add
+        # check that the right child is a Num
+        # check that the left child is another BinOp
+        # left child of the left child is a Num
+        # right child of the left child is a Num
+        #       +
+        #     /  \
+        #    *    4
+        #  /  \
+        # 2    3
+        if (type(node.op).__name__ == 'Add' and
+                type(node.right).__name__ == 'Num' and
+                type(node.left).__name__ == 'BinOp' and
+                type(node.left.op).__name__ == 'Mult' and
+                type(node.left.left).__name__ == 'Num' and
+                type(node.left.right).__name__ == 'Num'):
+            print('first')
+            left_left = self.visit(node.left.left)
+            left_right = self.visit(node.left.right)
+            right = self.visit(node.right)
+            return left_left * (left_right + right)
+        if (type(node.op).__name__ == 'Add' and
+                type(node.left).__name__ == 'Num' and
+                type(node.right).__name__ == 'BinOp' and
+                type(node.right.op).__name__ == 'Mult' and
+                type(node.right.left).__name__ == 'Num' and
+                type(node.right.right).__name__ == 'Num'):
+            print('second')
+            left = self.visit(node.left)
+            right_left = self.visit(node.right.left)
+            right_right = self.visit(node.right.right)
+            return (left + right_left) * right_right
+        else:
+            args = [self.visit(node.left), self.visit(node.right)]
+            op = self.visit(node.op)
+            return op(*args)
+    @staticmethod
+    def run(code):
+        BindingInterpreter().visit(parse(code))
+
 def main():
     arg_parser = ArgumentParser()
     arg_parser.add_argument('line_or_file')
-    arg_parser.add_argument('--interp', choices=('correct', 'stochastic'))
+    arg_parser.add_argument('--interp', choices=('correct', 'stochastic', 'binding'))
     args = arg_parser.parse_args()
     if file_exists(args.line_or_file):
         with open(args.line_or_file) as fd:
@@ -94,6 +138,8 @@ def main():
         interpreter = PyliteInterpreter
     elif args.interp == 'stochastic':
         interpreter = StochasticPyliteInterpreter
+    elif args.interp == 'binding':
+        interpreter = BindingInterpreter
     interpreter.run(parse(code))
 
 if __name__ == '__main__':
--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-print(2 * 2 + 4)
+print(2 * 3 + 4)
 print(2 + 3 * 4)
 print("hello world")
 a = 1
